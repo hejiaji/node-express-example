99cf6eb71edbf1517ee4acfcac1d45b9a5c05359
Jeremy He <jeremy.he@safetyculture.io>
2018-10-10 15:28:28 +1100

    restful http server

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const router = require('./router');
 
 app.use(router);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3077, () => console.log('Example app listening on port 3077!'));
--- router.js
+++ router.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const nocache = require('nocache');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const proxy = require('express-http-proxy');
 const router = express.Router();
 
 router.use((req, res, next) => {
   console.log('Time: ', new Date());
   next();
-});
+}, nocache(), bodyParser.json(), cors());
+
+let books = ['harry', 'potter'];
 
 router.get('/books/:bookId', (req, res) => {
   res.send(`Here is your book ${req.params.bookId}`);
+});
+
+router.post('/books', (req, res) => {
+  books = books.concat(req.body.name);
+  res.status(201);
+  res.send("");
+});
+
+router.get('/books', (req, res) => {
+  res.send(books);
 });
 
 module.exports = router;
